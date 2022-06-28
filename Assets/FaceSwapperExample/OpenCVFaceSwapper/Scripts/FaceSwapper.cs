b33c86115846bd0741892c8e5bcf5f4ae02642b7
EnoxSoftware <enox.software@gmail.com>
2022-06-28 23:33:15 +0900

    Change the line feed code [utf-8/lf]

--- Assets/FaceSwapperExample/OpenCVFaceSwapper/Scripts/FaceSwapper.cs
+++ Assets/FaceSwapperExample/OpenCVFaceSwapper/Scripts/FaceSwapper.cs
@@ -1,4 +1,4 @@
-﻿using OpenCVForUnity.CoreModule;
+using OpenCVForUnity.CoreModule;
 using OpenCVForUnity.ImgprocModule;
 using OpenCVForUnity.PhotoModule;
 using OpenCVForUnity.UtilsModule;
--- Assets/FaceSwapperExample/OpenCVFaceSwapper/Scripts/RectUtils.cs
+++ Assets/FaceSwapperExample/OpenCVFaceSwapper/Scripts/RectUtils.cs
@@ -1,4 +1,4 @@
-﻿using OpenCVForUnity.CoreModule;
+using OpenCVForUnity.CoreModule;
 using System;
 
 namespace OpenCVForUnity.FaceSwap
@@ -12,7 +12,7 @@ namespace OpenCVForUnity.FaceSwap
         /// <param name="x">The amount to inflate this Rectangle horizontally. </param>
         /// <param name="y">The amount to inflate this Rectangle vertically. </param>
         /// <returns></returns>
-        public static Rect Inflate (Rect rect, int x, int y)
+        public static Rect Inflate(Rect rect, int x, int y)
         {
             rect.x -= x;
             rect.y -= y;
@@ -27,17 +27,17 @@ namespace OpenCVForUnity.FaceSwap
         /// <param name="a">A rectangle to intersect. </param>
         /// <param name="b">A rectangle to intersect. </param>
         /// <returns></returns>
-        public static Rect Intersect (Rect a, Rect b)
+        public static Rect Intersect(Rect a, Rect b)
         {
-            int x1 = Math.Max (a.x, b.x);
-            int x2 = Math.Min (a.x + a.width, b.x + b.width);
-            int y1 = Math.Max (a.y, b.y);
-            int y2 = Math.Min (a.y + a.height, b.y + b.height);
+            int x1 = Math.Max(a.x, b.x);
+            int x2 = Math.Min(a.x + a.width, b.x + b.width);
+            int y1 = Math.Max(a.y, b.y);
+            int y2 = Math.Min(a.y + a.height, b.y + b.height);
 
             if (x2 >= x1 && y2 >= y1)
-                return new Rect (x1, y1, x2 - x1, y2 - y1);
+                return new Rect(x1, y1, x2 - x1, y2 - y1);
             else
-                return new Rect ();
+                return new Rect();
         }
 
         /// <summary>
@@ -46,14 +46,14 @@ namespace OpenCVForUnity.FaceSwap
         /// <param name="a">A rectangle to union. </param>
         /// <param name="b">A rectangle to union. </param>
         /// <returns></returns>
-        public static Rect Union (Rect a, Rect b)
+        public static Rect Union(Rect a, Rect b)
         {
-            int x1 = Math.Min (a.x, b.x);
-            int x2 = Math.Max (a.x + a.width, b.x + b.width);
-            int y1 = Math.Min (a.y, b.y);
-            int y2 = Math.Max (a.y + a.height, b.y + b.height);
+            int x1 = Math.Min(a.x, b.x);
+            int x2 = Math.Max(a.x + a.width, b.x + b.width);
+            int y1 = Math.Min(a.y, b.y);
+            int y2 = Math.Max(a.y + a.height, b.y + b.height);
 
-            return new Rect (x1, y1, x2 - x1, y2 - y1);
+            return new Rect(x1, y1, x2 - x1, y2 - y1);
         }
     }
 }
